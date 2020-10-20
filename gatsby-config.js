878313a3f1e58caa84223657843fad204ed10339
AlexShaposhnykov <alex.sh6789@gmail.com>
2020-10-20 13:40:11 +0300

    Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,16 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.API_URL || 'http://localhost:1337',
         queryLimit: 1000,
         contentTypes: [`portfolios`],
         singleTypes: [],
