310be36c3b9ee044ff4d66ab518277ac0d263d5f
AlexShaposhnykov <alex.sh6789@gmail.com>
2020-10-20 13:20:26 +0300

    Refresh cms

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd285e0213b78a5f2dea537aa677328f9'),
+    },
+  },
+});
